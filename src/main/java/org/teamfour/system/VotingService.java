8a879d33a166fd56604788eef29241ce2e67e158
Isaac <ifuegi@unm.edu>
2023-05-02 19:04:26 -0600

    stuff

---
+++ src/main/java/org/teamfour/system/VotingService.java
@@ -0,0 +1,32 @@
+package org.teamfour.system;
+
+import org.teamfour.model.db.Ballot;
+import org.teamfour.system.enums.Status;
+
+public class VotingService implements VotingSystem{
+    private Status status;
+    private Ballot ballot;
+    @Override
+    public Status getStatus() {
+        return status;
+    }
+    public boolean setElection(Ballot ballot, Integer[] Times){
+
+        return false;
+    }
+    public boolean voterSignIn(Integer IDNumber){
+        return false;
+    }
+    public boolean voteCasting(){
+        return false;
+    }
+    @Override
+    public Ballot getBallot() {
+        return ballot;
+    }
+
+    @Override
+    public void setStatus(Status statusIn) {
+        status = statusIn;
+    }
+}
